56bd8b8383f3e73116d0ba13468514e4ef792aa1
pito <pitocms@yahoo.com>
2024-06-10 00:49:58 +0600

    indent corr

--- src/models/Role.php
+++ src/models/Role.php
@@ -3,14 +3,14 @@ require_once __DIR__ . '/../config/database.php';
 
 class Role 
 {
-    private $conn;
-    private $table_name = 'roles';
+	private $conn;
+	private $table_name = 'roles';
 
-    public $id;
-    public $role_name;
-    public $description;
+	public $id;
+	public $role_name;
+	public $description;
 
-    public function __construct($db) {
-        $this->conn = $db;
-    }
+	public function __construct($db) {
+		$this->conn = $db;
+	}
 }
